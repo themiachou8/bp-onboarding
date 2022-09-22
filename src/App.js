ca5f630a89ef847cbf640f50f52f87708144d87d
Mia Chou <Miachou@berkeley.edu>
2022-09-21 18:21:48 -0700

    worked on Sunday Friends login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className = 'SmallerBox'>
+        <header className='Welcome'>
+          Welcome Back!
+        </header>
+        <p className = 'Sign-in'>
+          Sign in to continue
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className = 'emailBox'>
+          <p className = 'smallBox'>Email </p>
+          <input type='text' className = 'input' value="thisisanemail@email.com"></input>
+        </div>
+        <div className = 'emailBox'>
+          <p className = 'smallBox'>Password </p>
+          <input type='text' className = 'input' value="Create Password"></input>
+        </div>
+        <div className = 'bottom-row'>
+          <p className = 'text'>
+            Haven't activated your account yet? Activate now
+          </p>
+          <button className='sign-in'>Sign In</button>
+        </div>
+      </div>
     </div>
   );
 }
